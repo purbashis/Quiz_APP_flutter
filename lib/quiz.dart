338880182033583b09b92f6b3a439c0454f1acfa
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-10 18:08:01 +0530

    switching to the next screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/questions.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
 
@@ -10,7 +11,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  var activeScreen = StartScreen(); // you can store widget in a variable .
+  Widget? activeScreen;
+  // you can store widget in a variable .
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -22,7 +36,7 @@ class _QuizState extends State<Quiz> {
               Color.fromARGB(255, 135, 78, 235)
             ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
