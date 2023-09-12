43bac74b496fd992fd65b63ca12f1cc6061c1f10
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-12 20:51:29 +0530

    none

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,4 @@
-import 'package:adv_basics/questions.dart';
+import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
 
