073e1acd78d458c5e26fd0ad50113c0078a32eb6
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-13 14:40:19 +0530

    accessing list and object properties
    accessing list from adv_basics/data/questions and used Thoes in questionsScreen.dart

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,29 +13,35 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The Question..',
-            style: TextStyle(
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Colors.white,
             ),
           ),
           const SizedBox(height: 30),
           AnswerButton(
             onTap: () {},
-            answerText: 'Answer 1',
+            answerText: currentQuestion.answers[0],
           ),
           AnswerButton(
             onTap: () {},
-            answerText: 'Answer 2',
+            answerText: currentQuestion.answers[1],
           ),
           AnswerButton(
             onTap: () {},
-            answerText: 'Answer 3',
+            answerText: currentQuestion.answers[2],
+          ),
+          AnswerButton(
+            onTap: () {},
+            answerText: currentQuestion.answers[3],
           ),
         ],
       ),
