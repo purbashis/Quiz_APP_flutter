9c3c442d81efd8ca2c75bb11658703dc8cdc9aa1
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-13 19:06:20 +0530

    mutating values in memory ,using shuffle()

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -31,7 +31,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
-            ...currentQuestion.answers.map((answer) {
+            ...currentQuestion.getSuffledAnswers().map((answer) {
               return AnswerButton(answerText: answer, onTap: () {});
             }) //dynamically generated output list
             // AnswerButton(
