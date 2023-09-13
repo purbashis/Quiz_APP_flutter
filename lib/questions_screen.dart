0dbb1fe6064239affafc6d398c907ac36a91d040
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-13 19:19:22 +0530

    managing the questions index
    basically here what i did is incrementing the currentquestion index inside the setstate.also in answer button we pass the value od answer question

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -11,6 +11,15 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+  void answerQuestion() {
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    // currentQuestionIndex += 1;
+    setState(() {
+      currentQuestionIndex++; //increments the value by 1
+    });
+  }
+
   @override
   Widget build(context) {
     final currentQuestion = questions[0];
@@ -32,7 +41,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ),
             const SizedBox(height: 30),
             ...currentQuestion.getSuffledAnswers().map((answer) {
-              return AnswerButton(answerText: answer, onTap: () {});
+              return AnswerButton(
+                answerText: answer,
+                onTap: answerQuestion,
+              );
             }) //dynamically generated output list
             // AnswerButton(
             //   onTap: () {},
