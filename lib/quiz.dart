d540b8bf570f5aec2ec5c607f3384e450b62659b
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-14 21:39:03 +0530

    more conditions

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,9 +1,12 @@
-import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
+
 import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
+
   @override
   State<Quiz> createState() {
     return _QuizState();
@@ -11,54 +14,50 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswer = []; //adding elements to existing list
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
-  // you can store widget in a variable .
-// @override
-//   void initState() {
-//     activeScreen = StartScreen(switchScreen);
-//     super.initState();
-//   }
+
   void switchScreen() {
     setState(() {
-      activeScreen = 'question-screen';
+      activeScreen = 'questions-screen';
     });
   }
 
   void chooseAnswer(String answer) {
-    selectedAnswer.add(answer);
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
   Widget build(context) {
-    // Widget screenWidget = activeScreen == 'start-screen'
-    //     ? StartScreen(switchScreen)
-    //     : const QuestionsScreen();
-
     Widget screenWidget = StartScreen(switchScreen);
 
-    if (activeScreen == 'question-screen') {
-      setState(() {
-        screenWidget = QuestionsScreen(
-          onSelectAnswer: chooseAnswer,
-        );
-      });
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
     }
 
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 74, 19, 170),
-              Color.fromARGB(255, 159, 120, 226)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
           child: screenWidget,
-          //  child: activeScreen == 'start-screen'
-          // ? StartScreen(switchScreen)
-          // : const QuestionsScreen(),
-          //ternary expression
         ),
       ),
     );
