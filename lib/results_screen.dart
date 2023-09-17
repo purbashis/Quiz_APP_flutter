1224f252371098d0e3755c09e77e926578484a33
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-17 21:47:15 +0530

    maps and lists

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,8 +1,26 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.choseAnswers});
   final List<String> choseAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choseAnswers.length; i++) {
+      //loop body
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': choseAnswers[i]
+      });
+    }
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
