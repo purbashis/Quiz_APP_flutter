17577016b2eb1888505ffdebe23a2884b89df41e
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-19 10:03:13 +0530

    combining columns and rows in result screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -30,7 +30,6 @@ class _QuizState extends State<Quiz> {
 
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        selectedAnswers = [];
         activeScreen = 'results-screen';
       });
     }
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,5 @@
 import 'package:adv_basics/data/questions.dart';
-import 'package:flutter/foundation.dart';
+import 'package:adv_basics/questions_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
@@ -15,7 +15,7 @@ class ResultScreen extends StatelessWidget {
         'question_index': i,
         'question': questions[i].text,
         'correct_answer': questions[i].answers[0],
-        'user_answer': choseAnswers[i]
+        'user_answer': choseAnswers[i],
       });
     }
     return summary;
@@ -24,23 +24,27 @@ class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: double.infinity,
-        child: Container(
-            margin: const EdgeInsets.all(40),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text('You answered X  and Y questions correctly! '),
-              SizedBox(
-                height: 30,
-              ),
-              Text('List of answers and questions'),
-              SizedBox(
-                height: 30,
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text('Restart Quiz !'),
-              )
-            ])));
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You answered X out of Y questions correctly!'),
+            const SizedBox(
+              height: 30,
+            ),
+            QuestionsSummary(getSummaryData()),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Restart Quiz!'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
