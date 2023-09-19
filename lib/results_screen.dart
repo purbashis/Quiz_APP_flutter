cfad7bc6af9bd0336039e68b0efa9185262b683c
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-19 10:29:51 +0530

    filtering and analyzing
     'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!'

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -23,6 +23,11 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -30,7 +35,8 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly!'),
+            Text(
+                'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!'),
             const SizedBox(
               height: 30,
             ),
